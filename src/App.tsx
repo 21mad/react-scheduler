7f2f4082afda45949a65bc094629ea18f3f5cd3c
21mad <ilmir.abyzov@mail.ru>
2024-11-26 11:59:38 +0300

    add config for min/max zoom

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,12 @@ function App() {
           isLoading={false}
           onTileClick={handleTileClick}
           onFilterData={handleFilterData}
-          config={{ zoom: 0, maxRecordsPerPage: maxRecordsPerPage, showThemeToggle: true }}
+          config={{
+            zoom: 1,
+            maxRecordsPerPage: maxRecordsPerPage,
+            maxZoom: 1,
+            showThemeToggle: true
+          }}
           onItemClick={(data) => console.log("clicked: ", data)}
         />
       ) : (
